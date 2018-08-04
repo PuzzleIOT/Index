535eb3f0c6756ebfbcbfbee066bdee6ddc977f26
CompulsiveCoder <compulsivecoder@gmail.com>
2018-08-04 13:16:57 +1000

    Fixed init script

--- init-submodules.sh
+++ init-submodules.sh
@@ -2,18 +2,6 @@ git submodule update --init
 
 DIR=$PWD
 
-cd sketches/monitor/SoilMoistureSensorCalibratedSerial/ && \
+cd sketches/switch/NetSwitch/ && \
 sh init.sh && \
 cd $DIR && \
-
-cd sketches/monitor/SoilMoistureSensorCalibratedSerialESP/ && \
-sh init.sh && \
-cd $DIR && \
-
-cd sketches/irrigator/SoilMoistureSensorCalibratedPump/ && \
-sh init.sh && \
-cd $DIR && \
-
-cd sketches/irrigator/SoilMoistureSensorCalibratedPumpESP/ && \
-sh init.sh && \
-cd $DIR
