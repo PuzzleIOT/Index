bbf287369cbf45e37939db7e6f983dc0eed29b23
CompulsiveCoder <compulsivecoder@gmail.com>
2018-08-11 20:02:46 +0000

    Added missing file

---
+++ common-test-flags.sh
@@ -0,0 +1,24 @@
+MOCK_FLAG_FILE="is-mock-setup.txt"
+MOCK_HARDWARE_FLAG_FILE="is-mock-hardware.txt"
+MOCK_SUBMODULE_BUILDS_FLAG_FILE="is-mock-submodule-builds.txt"
+
+IS_MOCK_SETUP=0
+IS_MOCK_HARDWARE=0
+IS_MOCK_SUBMODULE_BUILDS=0
+
+IS_MOCK_SETUP=0
+if [ -f "$MOCK_FLAG_FILE" ]; then
+  IS_MOCK_SETUP=1
+  echo "Is mock setup"
+fi
+
+if [ -f "$MOCK_HARDWARE_FLAG_FILE" ]; then
+  IS_MOCK_HARDWARE=1
+  echo "Is mock hardware"
+fi
+
+if [ -f "$MOCK_SUBMODULE_BUILDS_FLAG_FILE" ]; then
+  IS_MOCK_SUBMODULE_BUILDS=1
+  echo "Is mock submodule builds"
+fi
+
